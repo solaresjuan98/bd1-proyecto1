5fe15a097fec087221977196f53cd3a77295ed72
Juan Solares <solaresjuan43@gmail.com>
2021-09-16 22:45:33 -0600

    fix query2

--- queries/query2.sql
+++ queries/query2.sql
@@ -1,11 +1,10 @@
 -- 2. Mostrar el nombre, apellido y pago total de todos los clientes que han rentado
 -- películas por lo menos 40 veces
-SELECT CLIENT_FIRST_NAME, CLIENT_LAST_NAME, count(*) as times_rented, sum(rd.RENT_COST) as total
-FROM CLIENT
-        JOIN MOVIE_RENTAL mr on CLIENT.CLIENT_CODE = mr.CLIENT_CODE
-        JOIN RENTAL_DETAIL rd on mr.RENTAL_CODE = rd.RENTAL_CODE
+select distinct cli.CLIENT_FIRST_NAME, cli.CLIENT_LAST_NAME, sum(MOUNT_TO_PAY)
+from MOVIE_RENTAL mov_rental
+        join CLIENT cli on cli.CLIENT_CODE = mov_rental.CLIENT_CODE
 having count(*) >= 40
-group by CLIENT_FIRST_NAME, CLIENT_LAST_NAME;
+group by cli.CLIENT_FIRST_NAME, cli.CLIENT_LAST_NAME;
 
 
 
